8c96149edb5da763f7e0e12a31b444ce2a901697
Marcos Velasco <devslife7@gmail.com>
2023-07-15 12:06:12 -0400

    added navbar and footer c components

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,6 @@
 import '../styles/globals.css'
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
 
 export const metadata = {
   title: 'dsbgeneralconstruction',
@@ -8,6 +10,7 @@ export const metadata = {
 type LayoutProps = {
   children: React.ReactNode
 }
+
 const RootLayout = ({ children }: LayoutProps) => {
   return (
     <html lang="en">
@@ -15,7 +18,11 @@ const RootLayout = ({ children }: LayoutProps) => {
         <div className="main">
           <div className="gradient" />
         </div>
-        <main className="app">{children}</main>
+        <main className="app">
+          <Navbar />
+          {children}
+          <Footer />
+        </main>
       </body>
     </html>
   )
