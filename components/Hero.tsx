08028848b15b7175c0d6f54466115a6c0a333c7c
Marcos Velasco <devslife7@gmail.com>
2023-07-19 15:02:56 -0400

    fixed the hero section css bug

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,12 +2,12 @@ import Link from "next/link"
 
 export default function Hero() {
   return (
-    <div className="h-screen bg-[url('../public/images/HeroSection.jpg')] bg-cover bg-fixed">
-      <div className="h-full bg-black bg-opacity-25">
-        <div className="container flex h-full items-center ">
-          <div className="relative rounded-md bg-black bg-opacity-50 px-10 py-20 text-white lg:left-[-4rem] lg:px-16">
-            <h1 className=" text-4xl font-bold md:text-5xl lg:text-6xl">Renovate Your Home</h1>
-            <div className="mg:leading-10  max-w-[53rem] pb-10 pt-5 text-lg font-light md:text-xl lg:text-2xl">
+    <div className="h-[70vh] lg:h-[80vh] bg-[url('../public/images/HeroSection.jpg')] bg-cover">
+      <div className="bg-black bg-opacity-25 h-full">
+        <div className="container h-full flex items-center text-white">
+          <div className="bg-black bg-opacity-50 px-8 lg:px-16 py-14 lg:py-20 relative lg:left-[-4rem] rounded-md">
+            <h1 className="font-bold text-5xl md:text-5xl lg:text-6xl">Renovate Your Home</h1>
+            <div className="max-w-[53rem] pb-10 pt-5 mg:leading-10 font-light text-lg md:text-xl lg:text-2xl">
               Our skilled artisans pour their heart and soul into every project, ensuring impeccable finishes
               and long-lasting beauty.
             </div>
