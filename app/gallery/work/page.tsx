b89de8ab6b12d69b9a085105afc2f2cc526b3524
Marcos Velasco <devslife7@gmail.com>
2023-07-21 16:50:00 -0400

    improving flex behavior

--- app/gallery/work/page.tsx
+++ app/gallery/work/page.tsx
@@ -5,7 +5,7 @@ export default function work() {
   return (
     <div className="container-custom mt-20 lg:flex">
       <div className="bg-green-300 lg:w-[35%]">comments</div>
-      <div className="bg-red-300 lg:w-[65%] flex flex-wrap order-first">
+      <div className="bg-red-300 lg:w-[65%] flex justify-center flex-wrap order-first my-20">
         <Image src={aboutPage} width={350} alt="image" />
         <Image src={aboutPage} width={350} alt="image" />
         <Image src={aboutPage} width={350} alt="image" />
