7a658f45a2343292ea6c851a927f7443e47b9e26
Marcos Velasco <devslife7@gmail.com>
2023-08-10 17:42:07 -0400

    improving hero section

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link"
 import { socialMediaLinks } from "../../data/socialMediaLinks"
 import { FaMapLocationDot } from "react-icons/fa6"
-import { BsWhatsapp, BsFillTelephoneOutboundFill } from "react-icons/bs"
+import { BsWhatsapp } from "react-icons/bs"
 import { MdMailOutline } from "react-icons/md"
 
 export default function Contact() {
@@ -15,13 +15,13 @@ export default function Contact() {
 
   return (
     <div className="container-custom mt-20">
-      <div className="mb-32 mt-12">
+      <div className="mb-32 mt-32">
         <h1 className="text-5xl font-bold">Schedule a Free Estimate</h1>
         <p className="mb-14 md:mb-20 mt-8 max-w-2xl text-[1.4rem] font-light text-gray-700">
           We are located in Fort Washington, Maryland. We service Northern Virginia and Southern Maryland
           including PG County, Calvert County, Anne Arundel County Charles County and St. Mary&apos;s County.
         </p>
-        <div className="flex flex-wrap gap-10 md:gap-16 mb-10">{renderMediaLinks()}</div>
+        <div className="flex flex-wrap gap-10 md:gap-16 mb-20">{renderMediaLinks()}</div>
         <a
           href="tel:999-999-9999"
           className="bg-primary text-white border-[3px] border-solid border-transparent inline-block rounded-md text-xl px-7 py-2 md:px-10 md:py-3 md:text-2xl
@@ -30,22 +30,22 @@ export default function Contact() {
           Call now
         </a>
       </div>
-      <div className="mb-36 space-y-16 lg:flex lg:flex-wrap lg:items-end lg:justify-between gap-5 text-white">
-        <div className="bg-black bg-opacity-80 mx-auto md:mx-0 px-10 py-10 h-[21rem] max-w-[23rem] md:w-[24rem] rounded-md flex flex-col justify-evenly">
+      <div className="mb-36 space-y-16 lg:flex lg:flex-wrap lg:items-end lg:justify-between gap-5">
+        <div className="bg-gray-200 bg-opacity-80 mx-auto md:mx-0 px-10 py-10 h-[21rem] max-w-[23rem] md:w-[24rem] rounded-md flex flex-col justify-evenly">
           <p className="text-[4.7rem] mb-7 text-green-600">
             <BsWhatsapp />
           </p>
           <div className="text-[2.2rem]">WhatsApp</div>
           <div className="text-2xl font-light">+1 999 999 9999</div>
         </div>
-        <div className="bg-black bg-opacity-80 mx-auto md:mx-0 px-10 py-10 h-[17rem] max-w-[23rem] md:w-[23rem] rounded-md flex flex-col justify-evenly order-first">
+        <div className="bg-gray-200 bg-opacity-80 mx-auto md:mx-0 px-10 py-10 h-[17rem] max-w-[23rem] md:w-[23rem] rounded-md flex flex-col justify-evenly order-first">
           <p className="text-5xl mb-10 text-blue-500">
             <MdMailOutline />
           </p>
           <div className="text-[1.8rem]">Send us an email</div>
           <div className="text-xl font-light">email123@gmail.com</div>
         </div>
-        <div className="bg-black bg-opacity-80 mx-auto md:mx-0 px-10 py-10 h-[16rem] max-w-[23rem] md:w-[23rem] rounded-md flex flex-col justify-evenly">
+        <div className="bg-gray-200 bg-opacity-80 mx-auto md:mx-0 px-10 py-10 h-[16rem] max-w-[23rem] md:w-[23rem] rounded-md flex flex-col justify-evenly">
           <p className="text-5xl mb-10 text-primary">
             <FaMapLocationDot />
           </p>
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -12,7 +12,7 @@ export default function Hero() {
         <div className="container-custom h-full flex items-center text-white">
           <div className="bg-black bg-opacity-50 px-8 lg:px-16 py-14 lg:py-20 relative rounded-md">
             <h1 className="font-bold text-5xl md:text-5xl lg:text-7xl">Renovate Your Home</h1>
-            <p className="max-w-[53rem] pb-10 pt-5 font-light text-lg md:text-xl lg:text-xl">
+            <p className="max-w-[53rem] pb-10 pt-5 font-light text-lg md:text-xl lg:text-xl lg:leading-relaxed">
               Our skilled artisans pour their heart and soul into every project, 
               ensuring impeccable finishes and long-lasting beauty.
             </p>
