5bba04f752b2f0fbf27684b317288891ded11d00
Marcos Velasco <devslife7@gmail.com>
2023-08-10 17:22:45 -0400

    improved hero button

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -6,22 +6,21 @@ export default function Hero() {
   return (
     <div className="mt-20 h-[70vh] relative">
       <div className="absolute h-full w-full -z-10">
-        <Image src={heroBackground} className="object-cover h-full w-full" alt="Colorful Kitchen" placeholder="blur" priority />
+        <Image src={heroBackground} className="object-cover h-full w-full" placeholder="blur" priority alt="Hero Kitchen"/>
       </div>
       <div className="bg-black bg-opacity-25 h-full">
         <div className="container-custom h-full flex items-center text-white">
           <div className="bg-black bg-opacity-50 px-8 lg:px-16 py-14 lg:py-20 relative rounded-md">
             <h1 className="font-bold text-5xl md:text-5xl lg:text-7xl">Renovate Your Home</h1>
-            <div className="max-w-[53rem] pb-10 pt-5 font-light text-lg md:text-xl lg:text-xl">
+            <p className="max-w-[53rem] pb-10 pt-5 font-light text-lg md:text-xl lg:text-xl">
               Our skilled artisans pour their heart and soul into every project, 
               ensuring impeccable finishes and long-lasting beauty.
-            </div>
+            </p>
             <Link
               href="/gallery"
-              className="bg-primary border-[3px] border-solid border-transparent inline-block rounded-md text-lg px-7 py-2 md:px-10 md:py-3 md:text-2xl
-              hover:border-solid hover:bg-transparent hover:border-[3px] hover:border-primary transition-all ease-in-out duration-300 "
+              className="bg-primary border-[3px] border-solid border-transparent inline-block rounded-md text-lg px-7 py-2 md:px-6 md:py-2 md:text-xl hover:border-solid hover:bg-transparent hover:border-[3px] hover:border-primary transition-all ease-in-out duration-300 "
             >
-              Explore Gallery
+               Gallery...
             </Link>
           </div>
         </div>
