790411f8d291e3691a24eeec538d3bb4602cc313
Marcos Velasco <devslife7@gmail.com>
2023-08-12 18:16:08 -0400

    added custom buttom

--- components/home/GallerySection.tsx
+++ components/home/GallerySection.tsx
@@ -1,7 +1,11 @@
+import Button from "../shared/Button"
+
 export default function GallerySection() {
   return (
     <div className="container-custom flex items-center justify-center bg-red-50 h-screen">
-      <div className="text-5xl">GallerySection</div>
+      {/* <div className="text-5xl">GallerySection</div> */}
+      <Button>Submit</Button>
+      {/* <button type="button">rawww</button> */}
     </div>
   )
 }
--- components/shared/Button.tsx
+++ components/shared/Button.tsx
@@ -1,7 +1,8 @@
-export default function Button({ children }: { children: any }) {
+export default function Button({ className, children, ...props }: { className?: any; children: any }) {
+  const customStyles = "py-2 px-5 bg-primary rounded-md text-white"
   return (
-    <div>
-      <button>{children}</button>
-    </div>
+    <button {...props} className={`${customStyles} ${className}`}>
+      {children}
+    </button>
   )
 }
