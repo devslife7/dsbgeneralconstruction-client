011d9ecbda42ae60d08a2e00fe819e9450f21a89
Marcos Velasco <devslife7@gmail.com>
2023-08-16 19:28:47 -0400

    fixed the capital letter on react icons

--- components/shared/MyModal.tsx
+++ components/shared/MyModal.tsx
@@ -1,6 +1,6 @@
 import { Dialog, Transition } from "@headlessui/react"
 import { Fragment } from "react"
-import { RiCloseFill } from "react-icons/Ri"
+import { RiCloseFill } from "react-icons/ri"
 
 export default function MyModal({
   closeModal,
@@ -29,7 +29,7 @@ export default function MyModal({
         </Transition.Child>
 
         <div className="fixed inset-0 overflow-y-auto">
-          <div className="flex min-h-full items-center justify-center p-4 text-center">
+          <div className="flex items-center justify-center min-h-full p-4 text-center">
             <Transition.Child
               as={Fragment}
               enter="ease-out duration-300"
@@ -39,11 +39,11 @@ export default function MyModal({
               leaveFrom="opacity-100 scale-100"
               leaveTo="opacity-0 scale-95"
             >
-              <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-lg bg-white p-6 text-left align-middle shadow-xl transition-all">
-                <Dialog.Title className="text-2xl font-medium leading-6 text-gray-900 flex justify-between text-center mb-8">
+              <Dialog.Panel className="w-full max-w-md p-6 overflow-hidden text-left align-middle transition-all transform bg-white rounded-lg shadow-xl">
+                <Dialog.Title className="flex justify-between mb-8 text-2xl font-medium leading-6 text-center text-gray-900">
                   {title}
                   <RiCloseFill
-                    className="text-2xl hover:bg-gray-100 hover:cursor-pointer rounded-md hover:text-red-500 text-gray-500"
+                    className="text-2xl text-gray-500 rounded-md hover:bg-gray-100 hover:cursor-pointer hover:text-red-500"
                     onClick={closeModal}
                   />
                 </Dialog.Title>
