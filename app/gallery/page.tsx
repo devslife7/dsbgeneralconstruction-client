7e93861cba78be14194754503a11ef790fa67485
Marcos Velasco <devslife7@gmail.com>
2023-09-01 16:41:41 -0400

    added right url

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -9,7 +9,7 @@ type GalleryProp = {
 import Work from "../../components/Work"
 
 export default async function page() {
-    const res = await fetch("http://localhost:3000/works")
+    const res = await fetch("https://dsbgeneralconstruction-api-production.up.railway.app/works")
     const gallery: GalleryProp[] = await res.json()
     return (
         <div className="container flex items-center justify-center h-screen px-4 mx-auto lg:px-8">
