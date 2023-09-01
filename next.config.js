cca227c95a67739be59aaf5b2267c5d1b8b9b149
Marcos Velasco <devslife7@gmail.com>
2023-08-31 20:50:34 -0400

    updated nextjs

--- next.config.js
+++ next.config.js
@@ -1,23 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false,
-  swcMinify: true,
-  optimizeFonts: true,
-  images: {
-    remotePatterns: [
-      {
-        hostname: "localhost",
-      },
-      {
-        protocol: "https",
-        hostname: "dsbgeneralconstruction-images-videos.s3.amazonaws.com",
-      },
-      {
-        protocol: "https",
-        hostname: "dsbgeneralconstruction-api-production.up.railway.app",
-      },
-    ],
-  },
+    reactStrictMode: false,
+    swcMinify: true,
+    optimizeFonts: true,
+    images: {
+        remotePatterns: [
+            {
+                hostname: "localhost",
+            },
+            {
+                protocol: "https",
+                hostname: "dsbgeneralconstruction-images-videos.s3.amazonaws.com",
+            },
+            {
+                protocol: "https",
+                hostname: "dsbgeneralconstruction-api-production.up.railway.app",
+            },
+        ],
+    },
 }
 
 module.exports = nextConfig
