fc931c551d4ff2fdc9e4b9e28d0e141ec41b3d89
Marcos Velasco <devslife7@gmail.com>
2023-09-01 16:35:36 -0400

    gave it types

--- components/Work.tsx
+++ components/Work.tsx
@@ -1,8 +1,17 @@
-export default function Work({ gallery }: any) {
+type GalleryProp = {
+    id: number
+    title: string
+    description: string
+    image_urls: string[]
+    ratings: number[]
+}
+
+export default function Work({ gallery }: { gallery: GalleryProp[] }) {
     console.log(">>>>gallery", gallery[0].title)
     return (
         <div>
             <div>Work</div>
+            <div>{gallery[0].title}</div>
         </div>
     )
 }
