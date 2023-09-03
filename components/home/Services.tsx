024c2034b3f49baebabe0245afc0631220ce3c31
Marcos Velasco <devslife7@gmail.com>
2023-09-02 21:42:16 -0400

    good progress

--- components/home/Services.tsx
+++ components/home/Services.tsx
@@ -1,38 +1,40 @@
 import { RxDotFilled } from "react-icons/rx"
 
 const servicesList = [
-  "Concrete",
-  "Remodeling",
-  "Fences",
-  "Flooring",
-  "Electrical",
-  "Carpentry",
-  "Hauling",
-  "Land Clearing",
-  "Painting",
-  "Moving",
-  "Landscaping",
-  "Plumbing",
+    "Concrete",
+    "Remodeling",
+    "Fences",
+    "Flooring",
+    "Electrical",
+    "Carpentry",
+    "Hauling",
+    "Land Clearing",
+    "Painting",
+    "Moving",
+    "Landscaping",
+    "Plumbing",
 ]
 
 export default function Services() {
-  const renderServices = () => {
-    return servicesList.map((serviceName, idx) => (
-      <>
-        <span key={idx} className="inline-block leading-relaxed">
-          {serviceName}
-        </span>
-        <RxDotFilled className="inline-block mx-2 text-primary" />
-      </>
-    ))
-  }
-  return (
-    <div className="flex flex-col items-center px-1">
-      <h2 className="block mt-10 mb-4 text-4xl text-gray-800 lg:mb-12 lg:mt-24 lg:text-5xl">Our Services</h2>
-      <div className="max-w-[70rem] text-gray-600 text-center text-xl lg:text-3xl">
-        {renderServices()}
-        <span>etc</span>
-      </div>
-    </div>
-  )
+    const renderServices = () => {
+        return servicesList.map((serviceName, idx) => (
+            <>
+                <span key={idx} className="inline-block leading-relaxed">
+                    {serviceName}
+                </span>
+                <RxDotFilled className="inline-block mx-2 text-primary" />
+            </>
+        ))
+    }
+    return (
+        <div className="px-5 lg:flex-col lg:items-center lg:flex">
+            <h2 className="block mt-10 mb-4 text-4xl text-gray-800 lg:mb-12 lg:mt-24 lg:text-5xl">
+                Our Services
+            </h2>
+            <div className="max-w-[70rem] text-gray-600 lg:text-center text-xl lg:text-3xl">
+                {renderServices()}
+                <span>etc</span>
+            </div>
+        </div>
+    )
 }
