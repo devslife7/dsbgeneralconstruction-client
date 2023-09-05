e7460bde7121d50b56c3e13ed6c4b3f67afa6d63
Marcos Velasco <devslife7@gmail.com>
2023-09-05 15:03:47 -0400

    added an about section

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,37 +1,35 @@
 import Image from "next/image"
-import AboutUs from "@/public/images/aboutPage.webp"
+import AboutPage from "@/public/images/about-page.webp"
+import Link from "next/link"
+import { cn } from "@/lib/utils"
+import { buttonStyles } from "@/components/ui/button"
 
 export default function About() {
     return (
-        <div className="flex items-center py-20 my-container">
-            <div className="flex flex-col md:flex-row">
-                <div className="lg:mb-0 md:w-1/2 mb-14">
-                    <Image
-                        src={AboutUs}
-                        alt="About Us"
-                        width={800}
-                        height={600}
-                        className="object-cover"
-                        priority
-                    />
-                </div>
-                <div className="flex items-center mx-auto md:w-1/2 md:pl-8 lg:max-w-xl">
-                    <div>
-                        <h2 className="mb-6 text-5xl font-medium">Our Story</h2>
-                        <div className="space-y-3 text-lg font-light opacity-70">
-                            <p>
-                                At DSB General Construction, we have been transforming houses into dream homes
-                                for many years. Our passion for design, craftsmanship, and customer
-                                satisfaction sets us apart in the industry.
-                            </p>
-                            <p>
-                                With a focus on quality and attention to detail, we take pride in delivering
-                                exceptional results. Whether it&apos;s a kitchen remodel, bathroom renovation,
-                                or a complete home makeover, we approach each project with dedication and
-                                commitment to creating spaces that our clients love to call home.
-                            </p>
-                        </div>
-                    </div>
+        <div className="pt-4 lg:py-20">
+            <div className="my-container text-center mb-8 lg:mb-20">
+                <h1 className="text-2xl text-center font-semibold opacity-80 mb-4">
+                    About DSB <br /> General Construction
+                </h1>
+                <p className="opacity-60 text-center font-light max-w-3xl mx-auto">
+                    At DSB General Construction, we focus on quality and attention to detail. Whether it's a
+                    kitchen remodel, bathroom renovation, or a complete home makeover, we approach each
+                    project with dedication and commitment to creating spaces that our clients love to call
+                    home. We take pride in delivering exceptional results
+                </p>
+            </div>
+            <div className="h-[20rem] lg:h-[40rem] relative">
+                <Image src={AboutPage} fill alt="About Us" className="object-cover fixed" />
+            </div>
+            <div className="text-center lg:mt-20 bg-black text-white py-10">
+                <h2 className="font-semibold text-2xl">
+                    Create your dream home. <br /> Tell us about your project <br /> today.
+                </h2>
+
+                <div className="my-container text-center">
+                    <Link href="/contact" className={cn(buttonStyles(), "font-light lg:text-lg mt-10")}>
+                        Get A Free Estimate
+                    </Link>
                 </div>
             </div>
         </div>
