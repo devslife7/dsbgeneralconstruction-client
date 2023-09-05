6a547936e137d5a4ba7224f20a06088b7a714272
Marcos Velasco <devslife7@gmail.com>
2023-09-05 11:50:47 -0400

    added text to hero

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -15,21 +15,11 @@ export default function Hero() {
                     alt="Hero Kitchen"
                     src={heroBackground}
                     fill
-                    sizes="(max-width: 768px) 100vw, (max-width: 1200px) 70vw, 30vw"
+                    sizes="100vw"
                     priority
                     className="object-cover -z-10"
                 />
-                {/* <Image
-                    alt="Hero Kitchen"
-                    src={heroBackground}
-                    placeholder="blur"
-                    quality={100}
-                    fill
-                    // sizes="(max-width: 768px) 100vw, (max-width: 1200px) 70vw, 60vw"
-                    priority
-                    className="object-cover -z-10"
-                /> */}
-                {/* <div className="absolute inset-0 flex items-center justify-center w-full h-full mx-auto text-center text-white lg:w-auto lg:text-left lg:justify-start">
+                <div className="absolute inset-0 flex items-center justify-center w-full h-full mx-auto text-center text-white lg:w-auto lg:text-left lg:justify-start">
                     <div className="max-w-[18rem] lg:max-w-2xl py-14 lg:py-20 lg:ml-10">
                         <p className="text-4xl font-semibold leading-tight font-roboto md:text-5xl lg:leading-tight lg:text-7xl">
                             Exceptional Home Remodeling & Renovations
@@ -41,7 +31,7 @@ export default function Hero() {
                             Explore Gallery
                         </Link>
                     </div>
-                </div> */}
+                </div>
             </div>
         </div>
     )
