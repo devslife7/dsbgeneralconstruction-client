29a13932901e83a70ebfabf732ef44fb7ec8f6b5
Marcos Velasco <devslife7@gmail.com>
2023-09-05 13:22:52 -0400

    added services section

--- components/home/Services.tsx
+++ components/home/Services.tsx
@@ -1,38 +1,47 @@
-const servicesList = [
-    "Concrete",
-    "Remodeling",
-    "Fences",
-    "Flooring",
-    "Electrical",
-    "Carpentry",
-    "Hauling",
-    "Land Clearing",
-    "Painting",
-    "Moving",
-    "Landscaping",
-    "Plumbing",
-]
+import kitchen from "@/public/images/kitchen-service.webp"
+import kitchenMobile from "@/public/images/kitchen-service-mobile.webp"
+import bathroom from "@/public/images/bathroom-service.webp"
+import bathroomMobile from "@/public/images/bathroom-service-mobile.webp"
+import interior from "@/public/images/interior-service.webp"
+import interiorMobile from "@/public/images/interior-service-mobile.webp"
+import Image from "next/image"
+import Link from "next/link"
+import { buttonStyles } from "../ui/button"
+import { cn } from "@/lib/utils"
 
 export default function Services() {
-    const renderServices = () => {
-        return servicesList.map((serviceName, idx) => (
-            <>
-                <span key={idx} className="inline-block leading-relaxed">
-                    {serviceName}
-                </span>
-                {/* <RxDotFilled className="inline-block mx-2 text-primary" /> */}
-            </>
-        ))
-    }
     return (
-        <div className="px-5 lg:flex-col lg:items-center lg:flex">
-            <h2 className="block mt-10 mb-4 text-4xl text-gray-800 lg:mb-12 lg:mt-24 lg:text-5xl">
-                Our Services
+        <div className="mb-10">
+            <h2 className="block mt-32 mb-12 text-3xl font-semibold text-center text-gray-800 lg:mb-12 lg:mt-24 lg:text-5xl">
+                Our Services Include
             </h2>
-            <div className="max-w-[70rem] text-gray-600 lg:text-center text-xl lg:text-3xl">
-                {renderServices()}
-                <span>etc</span>
+            <div className="grid lg:grid-cols-3 xl:container xl:mx-auto xl:px-8 lg:gap-6">
+                <div className="relative w-full h-[22rem] lg:h-[45rem]">
+                    <Image src={kitchen} alt="kitchen" fill className="hidden object-cover lg:block" />
+                    <Image src={kitchenMobile} alt="kitchen" fill className="object-cover lg:hidden" />
+                    <div className="absolute inset-0 flex items-center justify-center text-3xl font-medium text-center text-white bg-black/30">
+                        Kitchens
+                    </div>
+                </div>
+                <div className="relative w-full h-[22rem] lg:h-[45rem]">
+                    <Image src={bathroom} alt="kitchen" fill className="hidden object-cover lg:block" />
+                    <Image src={bathroomMobile} alt="kitchen" fill className="object-cover lg:hidden" />
+                    <div className="absolute inset-0 flex items-center justify-center text-3xl font-medium text-center text-white bg-black/30">
+                        Bathrooms
+                    </div>
+                </div>
+                <div className="relative w-full h-[22rem] lg:h-[45rem]">
+                    <Image src={interior} alt="kitchen" fill className="hidden object-cover lg:block" />
+                    <Image src={interiorMobile} alt="kitchen" fill className="object-cover lg:hidden" />
+                    <div className="absolute inset-0 flex items-center justify-center text-3xl font-medium text-center text-white bg-black/30">
+                        Interiors
+                    </div>
+                </div>
             </div>
+
+            <Link href="/services" className={cn(buttonStyles({ size: "lg" }), "font-light lg:text-lg mt-10")}>
+                See All Services
+            </Link>
         </div>
     )
 }
