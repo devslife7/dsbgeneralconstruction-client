8d3fac47d29e5ddc7c9edd17f9c308fa3e09c2b5
Marcos Velasco <devslife7@gmail.com>
2023-09-05 11:37:05 -0400

    optimized hero image

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -6,19 +6,30 @@ import { cn } from "@/lib/utils"
 
 export default function Hero() {
     return (
-        <div className=" h-[30rem] lg:h-[54rem] relative lg:container mx-auto lg:px-8">
-            <div className="w-full h-full lg:px-8 bg-black/40">
+        <div
+            className=" h-[30rem] lg:mx-auto lg:container lg:h-[54rem] lg:px-8
+        "
+        >
+            <div className="relative w-full h-full lg:px-8 bg-black/30">
                 <Image
+                    alt="Hero Kitchen"
+                    src={heroBackground}
+                    fill
+                    sizes="(max-width: 768px) 100vw, (max-width: 1200px) 70vw, 30vw"
+                    priority
+                    className="object-cover -z-10"
+                />
+                {/* <Image
                     alt="Hero Kitchen"
                     src={heroBackground}
                     placeholder="blur"
                     quality={100}
                     fill
-                    sizes="(max-width: 768px) 100vw, (max-width: 1200px) 70vw, 60vw"
+                    // sizes="(max-width: 768px) 100vw, (max-width: 1200px) 70vw, 60vw"
                     priority
-                    className="object-cover lg:px-8 -z-10"
-                />
-                <div className="absolute flex items-center justify-center w-full h-full mx-auto text-center text-white lg:w-auto lg:text-left lg:justify-start">
+                    className="object-cover -z-10"
+                /> */}
+                {/* <div className="absolute inset-0 flex items-center justify-center w-full h-full mx-auto text-center text-white lg:w-auto lg:text-left lg:justify-start">
                     <div className="max-w-[18rem] lg:max-w-2xl py-14 lg:py-20 lg:ml-10">
                         <p className="text-4xl font-semibold leading-tight font-roboto md:text-5xl lg:leading-tight lg:text-7xl">
                             Exceptional Home Remodeling & Renovations
@@ -30,7 +41,7 @@ export default function Hero() {
                             Explore Gallery
                         </Link>
                     </div>
-                </div>
+                </div> */}
             </div>
         </div>
     )
