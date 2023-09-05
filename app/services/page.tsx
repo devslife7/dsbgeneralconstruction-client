28720694488b490428d1767c8e82bb8d00c7fe56
Marcos Velasco <devslife7@gmail.com>
2023-09-05 16:39:23 -0400

    added all the photos to services

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -1,56 +1,45 @@
-import kitchenMobile from "@/public/images/kitchen-service-mobile.webp"
-import bathroomMobile from "@/public/images/bathroom-service-mobile.webp"
-import interiorMobile from "@/public/images/interior-service-mobile.webp"
 import Image from "next/image"
 import Link from "next/link"
 import { cn } from "@/lib/utils"
 import { buttonStyles } from "@/components/ui/button"
+import { servicesData } from "@/lib/data/services"
+
 export default function page() {
+    const renderServices = () => (
+        <div className="grid sm:grid-cols-2 lg:grid-cols-3 sm:gap-4 xl:container lg:mx-auto xl:px-8 max-sm:space-y-4">
+            {servicesData.map((service, idx) => {
+                return (
+                    <div key={idx} className="text-center sm:max-w-sm sm:pb-20 relative">
+                        <div className="h-[12rem] sm:h-[18rem] max-w-xl sm:max-w-sm relative mx-auto">
+                            <Image src={service.src} alt={service.title} fill className="object-cover" />
+                        </div>
+                        <div className="my-container hidden sm:block">
+                            <h2 className="opacity-80 text-2xl font-medium mb-6 mt-10">{service.title}</h2>
+                            <p className="opacity-60  font-light">{service.Description}</p>
+                        </div>
+
+                        <div className="my-container sm:hidden absolute inset-0 flex items-center justify-center bg-black/30 text-3xl text-white font-medium">
+                            {service.title}
+                        </div>
+                    </div>
+                )
+            })}
+        </div>
+    )
+
     return (
-        <div className="pt-4 lg:py-20 space-y-10">
+        <div className="pt-10 lg:py-20 space-y-10">
             <div className="my-container">
-                <h1 className="text-2xl text-center font-semibold opacity-80 mb-2 lg:text-4xl">
+                <h1 className="text-3xl text-center font-semibold opacity-80 mb-2 lg:text-4xl">
                     Our Services
                 </h1>
-                <p className="text-center opacity-60">Let us help you make your dreams a reality.</p>
+                <p className="text-center opacity-60 font-light">
+                    Let us help you make your dreams a reality.
+                </p>
             </div>
 
-            <div className="text-center sm:max-w-sm pb-20">
-                <div className="h-[18rem] max-w-xl sm:max-w-sm relative">
-                    <Image src={kitchenMobile} alt="kitchen" fill className="object-cover" />
-                </div>
-                <div className="my-container">
-                    <h2 className="opacity-80 text-2xl font-medium mb-6 mt-10">Kitchen</h2>
-                    <p className="opacity-60  font-light">
-                        Describe your image here. Use catchy text to tell people the story behind the photo.
-                        Go to “Manage Media” to add your content.
-                    </p>
-                </div>
-            </div>
-            <div className="text-center sm:max-w-sm pb-20">
-                <div className="h-[18rem] max-w-xl sm:max-w-sm relative">
-                    <Image src={kitchenMobile} alt="kitchen" fill className="object-cover" />
-                </div>
-                <div className="my-container">
-                    <h2 className="opacity-80 text-2xl font-medium mb-6 mt-10">Kitchen</h2>
-                    <p className="opacity-60  font-light">
-                        Describe your image here. Use catchy text to tell people the story behind the photo.
-                        Go to “Manage Media” to add your content.
-                    </p>
-                </div>
-            </div>
-            <div className="text-center sm:max-w-sm pb-20">
-                <div className="h-[18rem] max-w-xl sm:max-w-sm relative">
-                    <Image src={kitchenMobile} alt="kitchen" fill className="object-cover" />
-                </div>
-                <div className="my-container">
-                    <h2 className="opacity-80 text-2xl font-medium mb-6 mt-10">Kitchen</h2>
-                    <p className="opacity-60  font-light">
-                        Describe your image here. Use catchy text to tell people the story behind the photo.
-                        Go to “Manage Media” to add your content.
-                    </p>
-                </div>
-            </div>
+            {renderServices()}
+
             <div className="text-center lg:mt-20 bg-black text-white py-20">
                 <h2 className="font-semibold text-2xl">
                     Create your dream home. <br /> Tell us about your project <br /> today.
---
+++ lib/data/services.tsx
@@ -0,0 +1,87 @@
+import kitchenMobile from "@/public/images/kitchen-service-mobile.webp"
+import concrete from "@/public/images/services/concrete.jpg"
+import fences from "@/public/images/services/fences.jpg"
+import flooring from "@/public/images/services/flooring.jpg"
+import electrical from "@/public/images/services/electrical.jpg"
+import carpentry from "@/public/images/services/carpentry.jpg"
+import hauling from "@/public/images/services/hauling.jpg"
+import landClearing from "@/public/images/services/land-clearing.jpg"
+import painting from "@/public/images/services/painting.jpg"
+import movers from "@/public/images/services/movers.jpg"
+import landscaping from "@/public/images/services/landscaping.jpg"
+import plumbing from "@/public/images/services/plumbing.jpg"
+
+export const servicesData = [
+    {
+        title: "Remodeling",
+        Description:
+            "Describe your image here. Use catchy text to tell people the story behind the photo. Go to Manage Media” to add your content.",
+        src: kitchenMobile,
+    },
+    {
+        title: "Concrete",
+        Description:
+            "Describe your image here. Use catchy text to tell people the story behind the photo. Go to Manage Media” to add your content.",
+        src: concrete,
+    },
+    {
+        title: "Fences",
+        Description:
+            "Describe your image here. Use catchy text to tell people the story behind the photo. Go to Manage Media” to add your content.",
+        src: fences,
+    },
+    {
+        title: "Flooring",
+        Description:
+            "Describe your image here. Use catchy text to tell people the story behind the photo. Go to Manage Media” to add your content.",
+        src: flooring,
+    },
+    {
+        title: "Electrical",
+        Description:
+            "Describe your image here. Use catchy text to tell people the story behind the photo. Go to Manage Media” to add your content.",
+        src: electrical,
+    },
+    {
+        title: "Carpentry",
+        Description:
+            "Describe your image here. Use catchy text to tell people the story behind the photo. Go to Manage Media” to add your content.",
+        src: carpentry,
+    },
+    {
+        title: "Hauling",
+        Description:
+            "Describe your image here. Use catchy text to tell people the story behind the photo. Go to Manage Media” to add your content.",
+        src: hauling,
+    },
+    {
+        title: "Land Clearing",
+        Description:
+            "Describe your image here. Use catchy text to tell people the story behind the photo. Go to Manage Media” to add your content.",
+        src: landClearing,
+    },
+    {
+        title: "Painting",
+        Description:
+            "Describe your image here. Use catchy text to tell people the story behind the photo. Go to Manage Media” to add your content.",
+        src: painting,
+    },
+    {
+        title: "Moving",
+        Description:
+            "Describe your image here. Use catchy text to tell people the story behind the photo. Go to Manage Media” to add your content.",
+        src: movers,
+    },
+    {
+        title: "Landscaping",
+        Description:
+            "Describe your image here. Use catchy text to tell people the story behind the photo. Go to Manage Media” to add your content.",
+        src: landscaping,
+    },
+    {
+        title: "Plumbing",
+        Description:
+            "Describe your image here. Use catchy text to tell people the story behind the photo. Go to Manage Media” to add your content.",
+        src: plumbing,
+    },
+]
