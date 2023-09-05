c78d88515b9e9ec800275d2f2dca1a86de7240e5
Marcos Velasco <devslife7@gmail.com>
2023-09-04 22:17:06 -0400

    put back svg files

--- public/svgs/index.ts
+++ public/svgs/index.ts
@@ -1,5 +1,5 @@
-// export { default as StarFilledSVG } from "./starFilled.svg"
-// export { default as EditSVG } from "./edit.svg"
+export { default as StarFilledSVG } from "./starFilled.svg"
+export { default as EditSVG } from "./edit.svg"
 export { default as DeleteSVG } from "./delete.svg"
 export { default as GallerySVG } from "./gallery.svg"
 export { default as ReviewsSVG } from "./reviews.svg"
