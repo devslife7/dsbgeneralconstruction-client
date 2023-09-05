6557a571c84296c195b5ab2ff1ace4402cf8980f
Marcos Velasco <devslife7@gmail.com>
2023-09-05 12:09:07 -0400

    redone navbar

--- components/layout/MobileNav.tsx
+++ components/layout/MobileNav.tsx
@@ -1,3 +1,4 @@
+import { cn } from "@/lib/utils"
 import { CloseSVG, HomeWorkSVG } from "@/public/svgs"
 import { Dialog } from "@headlessui/react"
 import Link from "next/link"
@@ -26,24 +27,24 @@ export default function MobileNav({ mobileMenuOpen, setMobileMenuOpen, navLinks 
             open={mobileMenuOpen}
             onClose={setMobileMenuOpen}
         >
-            <div className="fixed inset-0 z-10" />
-            <Dialog.Panel className="fixed top-0 bottom-0 right-0 z-10 w-full overflow-y-auto bg-custom-black bg-opacity-[0.99] text-white px-6 py-6 max-w-[15rem] sm:ring-1 sm:ring-gray-900/10">
-                <div className="flex items-center justify-between">
+            <Dialog.Panel className="fixed top-0 bottom-0 right-0 z-10 w-full overflow-y-auto bg-custom-white px-6 py-6 max-w-[15rem] sm:ring-1 sm:ring-gray-900/10 ">
+                <div className="flex items-center justify-between mt-6 ">
                     <HomeWorkSVG className="text-4xl text-primary" />
-                    <button type="button" className="-m-2.5 rounded-md p-2.5 " onClick={closeMobileMenu}>
+                    <button type="button" className="rounded-md" onClick={closeMobileMenu}>
                         <span className="sr-only">Close menu</span>
-                        <CloseSVG className="w-6 h-6" aria-hidden="true" />
+                        <CloseSVG className="mr-1 text-3xl opacity-70" aria-hidden="true" />
                     </button>
                 </div>
                 <div className="flow-root mt-10">
                     <div className="-my-6 divide-y divide-gray-500/10">
-                        <div className="py-6 space-y-2">
+                        <div className="py-6">
                             {navLinks.map((link, index) => (
                                 <Link
                                     key={index}
-                                    className={`-mx-2 block rounded-lg px-3 py-4 font-medium leading-7 text-lg ${
-                                        pathname === link.href && "text-primary"
-                                    }`}
+                                    className={cn(
+                                        "-mx-2 block px-3 py-5 font-medium leading-7 text-lg text-center opacity-60",
+                                        { "text-primary opacity-100": pathname === link.href }
+                                    )}
                                     onClick={closeMobileMenu}
                                     href={link.href}
                                 >
