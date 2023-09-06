6ad70a9a2e2ddac6dde57d19b29c8698d7a167a2
Marcos Velasco <devslife7@gmail.com>
2023-09-06 18:53:36 -0400

    added patch endpoint

--- app/api/works/[id]/route.ts
+++ app/api/works/[id]/route.ts
@@ -8,3 +8,10 @@ export async function GET(request: Request, { params }: { params: { id: string }
     const work = await prisma.work.findUnique({ where: { id: Number(id) } })
     return NextResponse.json(work)
 }
+
+export async function PATCH(request: Request, { params }: { params: { id: string } }) {
+    const id = params.id
+    const body = await request.json()
+    const work = await prisma.work.update({ where: { id: Number(id) }, data: body })
+    return NextResponse.json(work)
+}
