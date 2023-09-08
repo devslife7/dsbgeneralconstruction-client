0214f4c45e425ab1ff7372690bf400187ca6e451
Marcos Velasco <devslife7@gmail.com>
2023-09-08 11:00:44 -0400

    fixed the prisma erro

--- lib/api_calls/reviews.tsx
+++ lib/api_calls/reviews.tsx
@@ -1,22 +1,22 @@
-const serverURL = process.env.NEXT_PUBLIC_SERVER_URL
-const worksURL = serverURL + "/works/"
-const ratingURL = worksURL + "/ratings/"
-const uploadFilesURL = serverURL + "/upload_files/"
-const commentsURL = serverURL + "/comments/"
+// const serverURL = process.env.NEXT_PUBLIC_SERVER_URL
+// const worksURL = serverURL + "/works/"
+// const ratingURL = worksURL + "/ratings/"
+// const uploadFilesURL = serverURL + "/upload_files/"
+// const commentsURL = serverURL + "/comments/"
 
-export async function createReview(requestOBJ: any, workId: number) {
-    const requestURL = worksURL + workId + "/reviews"
-    const res = await fetch(requestURL, {
-        method: "POST",
-        body: JSON.stringify(requestOBJ),
-    })
-    return res.json()
-}
+// export async function createReview(requestOBJ: any, workId: number) {
+//     const requestURL = worksURL + workId + "/reviews"
+//     const res = await fetch(requestURL, {
+//         method: "POST",
+//         body: JSON.stringify(requestOBJ),
+//     })
+//     return res.json()
+// }
 
-export async function deleteReview(reviewId: number) {
-    const deleteURL = worksURL + "/id/reviews/" + reviewId
-    const res = await fetch(deleteURL, {
-        method: "DELETE",
-    })
-    return res.json()
-}
+// export async function deleteReview(reviewId: number) {
+//     const deleteURL = worksURL + "/id/reviews/" + reviewId
+//     const res = await fetch(deleteURL, {
+//         method: "DELETE",
+//     })
+//     return res.json()
+// }
--- lib/api_calls/works.tsx
+++ lib/api_calls/works.tsx
@@ -1,20 +1,20 @@
-const serverURL = process.env.NEXT_PUBLIC_SERVER_URL
-const worksURL = serverURL + "/works/"
-const ratingURL = worksURL + "/ratings/"
-const uploadFilesURL = serverURL + "/upload_files/"
-const commentsURL = serverURL + "/comments/"
+// const serverURL = process.env.NEXT_PUBLIC_SERVER_URL
+// const worksURL = serverURL + "/works/"
+// const ratingURL = worksURL + "/ratings/"
+// const uploadFilesURL = serverURL + "/upload_files/"
+// const commentsURL = serverURL + "/comments/"
 
-export async function fetchGallery() {
-    const res = await fetch(worksURL)
-    return res.json()
-}
-export async function deleteWork(workId: number) {
-    const deleteURL = worksURL + workId
-    const res = await fetch(deleteURL, {
-        method: "DELETE",
-    })
-    return res.json()
-}
+// export async function fetchGallery() {
+//     const res = await fetch(worksURL)
+//     return res.json()
+// }
+// export async function deleteWork(workId: number) {
+//     const deleteURL = worksURL + workId
+//     const res = await fetch(deleteURL, {
+//         method: "DELETE",
+//     })
+//     return res.json()
+// }
 
 // export const createWork = async (formData: any) => {
 //     return await axios.post(worksURL, formData)
