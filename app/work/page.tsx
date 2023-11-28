d4c9b2f0eaa468f69acdf7467431cf44e059a850
Marcos Velasco <devslife7@gmail.com>
2023-11-28 14:07:31 -0500

    added test component

--- app/work/page.tsx
+++ app/work/page.tsx
@@ -1,15 +1,18 @@
 import AddWorkButton from "@/components/work/addWorkButton"
-import Gallery from "@/components/work/gallery"
 import { Suspense } from "react"
 import { createWork } from "@/lib/models/work"
+import Gallery from "@/components/work/gallery"
+import CreatePostForm from "@/components/work/CreatePostForm"
 
 export default function page() {
-    return (
-        <>
-            <Suspense fallback={<div>Loading...</div>}>
-                <Gallery />
-            </Suspense>
-            <AddWorkButton createWork={createWork} />
-        </>
-    )
+  const testUser = {}
+  return (
+    <>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Gallery />
+      </Suspense>
+      <AddWorkButton createWork={createWork} />
+      <CreatePostForm user={testUser} />
+    </>
+  )
 }
--- components/work/addWorkButton.tsx
+++ components/work/addWorkButton.tsx
@@ -3,37 +3,37 @@ import CreateWorkForm from "./createWorkForm"
 import { PlusSVG } from "@/public/svgs"
 import { useState } from "react"
 import {
-    Dialog,
-    DialogContent,
-    DialogDescription,
-    DialogHeader,
-    DialogTitle,
-    DialogTrigger,
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
 } from "@/components/ui/dialog"
 
 export default function AddWorkButton({ createWork }: { createWork: (data: any) => void }) {
-    const [openDialog, setOpenDialog] = useState(false)
+  const [openDialog, setOpenDialog] = useState(false)
 
-    const closeDialog = () => setOpenDialog(false)
+  const closeDialog = () => setOpenDialog(false)
 
-    return (
-        <div className=" my-container">
-            <Dialog open={openDialog} onOpenChange={setOpenDialog}>
-                <DialogTrigger
-                    onClick={() => setOpenDialog(true)}
-                    className="p-4 bg-gray-300 rounded-full text-white hover:bg-gray-400"
-                >
-                    <PlusSVG />
-                </DialogTrigger>
-                <DialogContent>
-                    <DialogHeader>
-                        <DialogTitle className="mb-4 text-xl font-medium">Add Work</DialogTitle>
-                        <DialogDescription>
-                            <CreateWorkForm createWork={createWork} closeDialog={closeDialog} />
-                        </DialogDescription>
-                    </DialogHeader>
-                </DialogContent>
-            </Dialog>
-        </div>
-    )
+  return (
+    <div className=" my-container">
+      <Dialog open={openDialog} onOpenChange={setOpenDialog}>
+        <DialogTrigger
+          onClick={() => setOpenDialog(true)}
+          className="p-4 bg-gray-300 rounded-full text-white hover:bg-gray-400"
+        >
+          <PlusSVG />
+        </DialogTrigger>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle className="mb-4 text-xl font-medium">Add Work</DialogTitle>
+            <DialogDescription>
+              <CreateWorkForm createWork={createWork} closeDialog={closeDialog} />
+            </DialogDescription>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
+    </div>
+  )
 }
