b49f070572cce28a7ef625b3351c134251e61bf6
Marcos Velasco <devslife7@gmail.com>
2023-11-29 12:33:34 -0500

    uploading works

--- lib/actions.ts
+++ lib/actions.ts
@@ -4,6 +4,9 @@ import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3"
 
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner"
 
+import crypto from "crypto"
+const generateFileName = (bytes = 32) => crypto.randomBytes(bytes).toString("hex")
+
 const s3 = new S3Client({
   region: process.env.AWS_BUCKET_REGION!,
   credentials: {
@@ -12,18 +15,34 @@ const s3 = new S3Client({
   },
 })
 
-export async function getSignedURL() {
+const acceptedTypes = [
+  "image/jpeg",
+  "image/jpg",
+  "image/png",
+  "image/webp",
+  "image/gif",
+  "video/mp4",
+  "video/webm",
+]
+
+const maxFileSize = 1024 * 1024 * 10 // 10MB
+
+export async function getSignedURL(type: string, size: number, checksum: string) {
   // make sure user is authenticated
   // const session = await auth()
   const session = true
+
   if (!session) return { error: "Not authenticated" }
+  if (!acceptedTypes.includes(type)) return { error: "File type not accepted" }
+  if (size > maxFileSize) return { error: "File size too large" }
 
   const putObjectCommand = new PutObjectCommand({
     Bucket: process.env.AWS_BUCKET_NAME!,
-    Key: "test.txt",
+    Key: generateFileName(),
+    ContentType: type,
+    ContentLength: size,
+    ChecksumSHA256: checksum,
   })
-
-  console.log("env***********", process.env.AWS_SECRET_ACCESS_KEY)
 
   const signedURL = await getSignedUrl(s3, putObjectCommand, {
     expiresIn: 60,
