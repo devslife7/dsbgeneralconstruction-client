9fefeac46b1d6342e3bbf29c4f43bb7386fa2503
Marcos Velasco <devslife7@gmail.com>
2023-11-29 10:14:01 -0500

    addec use server to actions

--- lib/actions.ts
+++ lib/actions.ts
@@ -1,12 +1,13 @@
+"use server"
 // import { auth } from "@/auth"
 import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3"
 
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner"
 
 const s3 = new S3Client({
-  region: process.env.AWS_BUCKET_REGION,
+  region: process.env.AWS_BUCKET_REGION!,
   credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
+    accessKeyId: process.env.AWS_ACCESS_KEY!,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
   },
 })
@@ -22,9 +23,12 @@ export async function getSignedURL() {
     Key: "test.txt",
   })
 
+  console.log("env***********", process.env.AWS_SECRET_ACCESS_KEY)
+
   const signedURL = await getSignedUrl(s3, putObjectCommand, {
     expiresIn: 60,
   })
 
   return { success: { url: signedURL } }
+  // return { success: { url: "signedURL" } }
 }
